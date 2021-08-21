bfd5a6f55d199f471f53871b34dedea1eca83afa
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-08-21 13:48:16 +0300

    adding more details button

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,11 +8,11 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[]=[
-    new Quote(1,'The purpose of our lives is to be happy.','Dalai Lama'),
-    new Quote(2,'You only live once, but if you do it right, once is enough.','Mae West'),
-    new Quote(3,'Get busy living or get busy dying.','Stephen King'),
-    new Quote(4,'Life is what happens when you’re busy making other plans','John Lennon'),
-    new Quote(5,'Never say die','Roman King')
+    new Quote(1,'"The purpose of our lives is to be happy."','Dalai Lama'),
+    new Quote(2,'"You only live once, but if you do it right, once is enough."','Mae West'),
+    new Quote(3,'"Get busy living or get busy dying."','Stephen King'),
+    new Quote(4,'"Life is what happens when you’re busy making other plans"','John Lennon'),
+    new Quote(5,'"Never say die','Roman King"')
   ];
   constructor() { }
 
